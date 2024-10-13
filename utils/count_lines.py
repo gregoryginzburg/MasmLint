1ae6b218e4c790101ed729f2168e1f169abfb46e
Gregory Ginzburg <grigorii.ginzburg@gmail.com>
2024-10-13 03:36:01 +0300

    remove comment badge

--- utils/count_lines.py
+++ utils/count_lines.py
@@ -8,6 +8,7 @@ def count_lines(filename):
 def total_lines_in_directory(directory='.'):
     """Recursively count lines in all .txt, .cpp, and .h files in the given directory and subdirectories."""
     total_lines = 0
+    final_str = ""
 
     for root, subfolders, files in os.walk(directory):
         # print(root, subfolders, files)
@@ -19,19 +20,29 @@ def total_lines_in_directory(directory='.'):
             subfolders.remove('ext')
         for file in files:
             # file.endswith(('.txt', '.cpp', '.h', '.cmake'))
-            if not file.endswith(('.md')):
+            if file.endswith(('.cpp', '.h')):
                 file_path = os.path.join(root, file)
                 try:
                     t = count_lines(file_path)
                     print(file_path, ": ", t)
                     total_lines += t
+                    
+                    # for gpt
+                    final_str += f"File {file}:\n\n"
+                    with open(file_path) as file:
+                        final_str += file.read()
+                    final_str += "\n"
+                    
                 except Exception as e:
                     print(f"Error reading {file_path}: {e}")
 
-    return total_lines
+    return total_lines, final_str
 
 
 if __name__ == "__main__":
     directory = os.getcwd()  # get current directory
-    directory += '\\..'
-    print(f"Total number of lines in .txt, .cpp, and .h files in '{directory}' and its subdirectories: {total_lines_in_directory(directory)}")
+    # directory += '\\..'
+    total_lines, final_str = total_lines_in_directory(directory)
+    with open("utils/full_code.txt", mode='w') as file:
+        file.write(final_str)
+    print(f"Total number of lines in .txt, .cpp, and .h files in '{directory}' and its subdirectories: {total_lines}")
