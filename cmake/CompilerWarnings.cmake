8108f98bfdbabf04f14568e888ec596c7bd9fb4d
Gregory Ginzburg <grigorii.ginzburg@gmail.com>
2025-01-15 02:07:21 +0300

    fix

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -37,7 +37,6 @@ function(
         -Wextra # reasonable and standard
         -Wshadow # warn the user if a variable declaration shadows one from a parent context
         -Wno-shadow-field-in-constructor # enable shadowing for parameters in constructor https://stackoverflow.com/questions/48888177/shadowing-and-gcc-wshadow
-        -Wno-shadow-field
         -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
         # catch hard to track down memory errors
         -Wold-style-cast # warn for c-style casts
@@ -54,7 +53,24 @@ function(
     )
 
     set(GCC_WARNINGS
-        ${CLANG_WARNINGS}
+        -Wall
+        -Wextra # reasonable and standard
+        # -Wshadow # turn off for now because gcc doesn't have Wno-shadow-field-in-constructor
+        -Wno-shadow-field-in-constructor # enable shadowing for parameters in constructor https://stackoverflow.com/questions/48888177/shadowing-and-gcc-wshadow
+        -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+        # catch hard to track down memory errors
+        -Wold-style-cast # warn for c-style casts
+        -Wcast-align # warn for potential performance problem casts
+        -Wunused # warn on anything being unused
+        -Woverloaded-virtual # warn if you overload (not override) a virtual function
+        -Wpedantic # warn if non-standard C++ is used
+        -Wconversion # warn on type conversions that may lose data
+        -Wsign-conversion # warn on sign conversions
+        -Wnull-dereference # warn if a null dereference is detected
+        -Wdouble-promotion # warn if float is implicit promoted to double
+        -Wformat=2 # warn on security issues around functions that format output (ie printf)
+        -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+
         -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
         -Wduplicated-cond # warn if if / else chain has duplicated conditions
         -Wduplicated-branches # warn if if / else branches have duplicated code
